028f5784b7bbc456e6ccbb36edab33f7996fe6db
Cory G Watson <gphat@onemogin.com>
2016-03-06 18:00:29 -0600

    More fixes.

--- app/controllers/Ticket.scala
+++ app/controllers/Ticket.scala
@@ -27,7 +27,7 @@ class Ticket @Inject() (val messagesApi: MessagesApi) extends Controller with I1
   val linkForm = Form(
     mapping(
       "link_type_id"-> longNumber,
-      "ticket" -> play.api.data.Forms.list(nonEmptyText),
+      "ticket"  -> play.api.data.Forms.list(nonEmptyText),
       "comment" -> optional(text)
     )(models.MakeLink.apply)(models.MakeLink.unapply)
   )
@@ -45,10 +45,10 @@ class Ticket @Inject() (val messagesApi: MessagesApi) extends Controller with I1
       "typeId"     -> longNumber,
       "priorityId" -> longNumber,
       "severityId" -> longNumber,
-      "summary"     -> nonEmptyText,
-      "description" -> optional(text),
+      "summary"    -> nonEmptyText,
+      "description"-> optional(text),
       "assigneeId" -> optional(longNumber),
-      "position"    -> optional(longNumber)
+      "position"   -> optional(longNumber)
     )(models.NewTicket.apply)(models.NewTicket.unapply)
   )
 
@@ -71,6 +71,35 @@ class Ticket @Inject() (val messagesApi: MessagesApi) extends Controller with I1
     )(models.TicketData.apply)(models.TicketData.unapply)
   )
 
+  def add = IsAuthenticated() { implicit request =>
+
+    ticketForm.bindFromRequest.fold(
+      errors => {
+        val projs = ProjectModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
+        val users = UserModel.getAll.map { x => (x.id.get.toString -> x.realName) }
+        val assignees = DefaultAssignee.values.map { x => (x.id.toString -> Messages(x.toString)) }
+        val ttypes = TicketTypeModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
+        val prios = TicketPriorityModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
+        val sevs = TicketSeverityModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
+
+        // TODO NONE
+        BadRequest(views.html.ticket.create(errors, projs, assignees.toList, projs, ttypes, prios, sevs, None))
+      },
+      value => {
+        TicketModel.create(request.user.id.get, value.projectId, value.typeId, value.priorityId, value.severityId, value.summary, value.description, value.assigneeId).fold(
+          error => {
+            // TODO FIX
+            Redirect(routes.Core.index()).flashing("error" -> "ticket.add.failure")
+          },
+          ticket => {
+            Stats.addEvent("ticketsCreated", Map("projectId" -> ticket.id.toString))
+            Redirect(routes.Ticket.item(id = ticket.ticketId)).flashing("success" -> "ticket.add.success")
+          }
+        )
+      }
+    )
+  }
+
   /**
    * Display a form for creating a ticket.
    * If we received a projectId then we will default the form to the appropriate
@@ -78,11 +107,11 @@ class Ticket @Inject() (val messagesApi: MessagesApi) extends Controller with I1
    */
   def create(projectId: Option[Long]) = IsAuthenticated() { implicit request =>
 
-    // Should be i18ned in the view
-    val projs = ProjectModel.getAll
-    val ttypes = Json.toJson(TicketTypeModel.getAll).toString
-    val prios = Json.toJson(TicketPriorityModel.getAll).toString
-    val sevs = Json.toJson(TicketSeverityModel.getAll).toString
+    // TODO Should be i18ned in the view
+    val projs = ProjectModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
+    val ttypes = TicketTypeModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
+    val prios = TicketPriorityModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
+    val sevs = TicketSeverityModel.getAll.map { x => (x.id.get.toString -> Messages(x.name)) }
 
     // The worst case scenario, just the user id
     val startTicket = models.NewTicket(
@@ -96,18 +125,13 @@ class Ticket @Inject() (val messagesApi: MessagesApi) extends Controller with I1
 
     val maybeProject: Option[models.Project] = if(projectId.isDefined) {
       ProjectModel.getById(projectId.get)
-    } else if(projs.length == 1) {
-      // If they've only got one project return that to save some trouble
-      Some(projs(0))
     } else {
       None
     }
 
-    val assignees = Json.toJson(UserModel.getAssignable()).toString
+    val assignees = UserModel.getAssignable().map { x => (x.id.getOrElse("").toString -> x.realName) }
 
-    val pj: String = maybeProject.map({ p => Json.toJson(p).toString }).getOrElse("{}")
-
-    Ok(views.html.ticket.create(ticketForm, assignees, Json.toJson(projs).toString, ttypes, prios, sevs, pj))
+    Ok(views.html.ticket.create(ticketForm, projs, assignees, projs, ttypes, prios, sevs, maybeProject))
   }
 
   def edit(ticketId: String) = IsAuthenticated() { implicit request =>
